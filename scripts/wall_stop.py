5d0573381b14d0cb7e56645d06f250130b740c0a
Yuki Iida <yuki_iida@cit-ar-lab.tech>
2017-06-06 15:51:45 +0900

    Add a test scripts

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('/motor_on',Trigger).call()
-    WallStop().run()
+    WallTrace().run()
 
