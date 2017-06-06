d12854ca1dd1c74755c723e16ad73d27159f102e
Yuki Iida <yuki_iida@cit-ar-lab.tech>
2017-06-06 15:47:06 +0900

    Add a test scripts

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -4,14 +4,14 @@ from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallStop():
-    def __int__(self):
+class WallTrace():
+    def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
         self.sensor_values = LightSensorValues()
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
 
-    def callback(self,messages):
+    def callback_lightsensors(self,messages):
         self.sensor_values = messages
 
     def run(self):
@@ -19,12 +19,17 @@ class WallStop():
         data = Twist()
    
         while not rospy.is_shutdown():
-            data.linear.x = 0.2 if self.sensor_values.sum_all < 500 else 0.0
+            data.linear.x = 0.2 
+            data.angular.z = 0
+            if self.sensor_values.sum_all >= 500:
+                data.linear.x = 0
+                data.angular.z = 0
+
             self.cmd_vel.publish(data)
             rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('wall_stop')
+    rospy.init_node('wall_trace')
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
